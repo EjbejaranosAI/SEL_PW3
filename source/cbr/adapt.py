1593e37e7a784b41ee41cb8f866dfd7a0f4ba888
Yazmina <yazmina.zurita@gmail.com>
2022-06-03 13:58:19 +0200

    First version to deal with ingredient exclusion

--- source/cbr/adapt.py
+++ source/cbr/adapt.py
@@ -1,19 +1,21 @@
 import os
 from pathlib import Path
 import sys
+
 sys.path.append('..')
 
 import random
+from xml.etree.ElementTree import SubElement
 from utils.helper import read_xml
 
 
 def random_recipe(path):
     root = read_xml(path)
-    #xml_recipe = root[random.randint(0, len(root)-1)]
+    # xml_recipe = root[random.randint(0, len(root)-1)]
     # recipe = {}
     # for att in xml_recipe:
     #     recipe[att.tag] = att.text
-    return root[random.randint(0, len(root)-1)]
+    return root[random.randint(0, len(root) - 1)]
 
 
 def adapt_glass(glass, recipe):
@@ -29,7 +31,42 @@ def subsumed(a, b):
     return a in b
 
 
-def adapt(query, recipe):
+def adapt_alc_type(alc, recipe):
+    pass
+
+
+def adapt_basic_taste(taste, recipe):
+    pass
+
+
+def include_ingredient(ing, recipe):
+    pass
+
+
+def replace_ingredient(ingr1, ingr2, recipe):
+    if subsumed(ingr1.attrib["basic_taste"], ingr2.attrib["basic_taste"]) \
+            and subsumed(ingr1.attrib["alc_type"], ingr2.attrib["alc_type"]):
+        recipe.find("ingredients/ingredient[.=ingr1.text]").text = ingr2.text
+        return True
+    return False
+
+
+def exclude_ingredient(exc_ingr, inc_ingrs, recipes):
+    replaced = False
+    for ingr in inc_ingrs:
+        replaced = replace_ingredient(exc_ingr, ingr, recipes[0])
+        if replaced:
+            return
+    for recipe in recipes:
+        for ingr in recipe.findall("ingredients/ingredient"):
+            replaced = replace_ingredient(exc_ingr, ingr, recipes[0])
+            if replaced:
+                return
+    recipes[0].findall("ingredients/ingredient").remove(recipes[0].find("ingredients/ingredient[.=exc_ingr.text]"))
+
+
+def adapt(query, recipes):
+    recipe = recipes[0]
     alc_types = set()
     basic_tastes = set()
     ingredients = set()
@@ -40,15 +77,31 @@ def adapt(query, recipe):
             basic_tastes.add(ing.attrib["basic_taste"])
         ingredients.add(ing.text)
 
+    exc_ingrs = query["exclude_ingredients"]
+
+    for exc_ingr in exc_ingrs:
+        if subsumed(exc_ingr, ingredients):
+            exclude_ingredient(exc_ingr, recipe)
+
     if not subsumed(query["category"], recipe.find("category").text):
         adapt_category(query["category"], recipe)
 
     if not subsumed(query["glass"], recipe.find("glass").text):
         adapt_glass(query["glass"], recipe)
 
-    # for alc in query["alc_type"]:
-    #      if not subsumed(alc, alc_types):
-    #          adapt_alc_type(alc, recipe)
+    for alc in query["alc_type"]:
+        if not subsumed(alc, alc_types):
+            adapt_alc_type(alc, recipe)
+
+    for taste in query["basic_tastes"]:
+        if not subsumed(taste, basic_tastes):
+            adapt_basic_taste(taste, recipe)
+
+    for ingr in query["ingredients"]:
+        if not subsumed(ingr, ingredients):
+            include_ingredient(ingr, recipe)
+
+    return recipe[0]
 
 
 if __name__ == "__main__":
@@ -61,7 +114,5 @@ if __name__ == "__main__":
              "ingredients": ["whisky", "cherry liqueur"],
              "exclude_ingredients": ["dry vermouth", "lemon peel"]
              }
-    recipe = random_recipe(xml_file)
-    adapt(query, recipe)
-    print(recipe)
-
+    recipes = [random_recipe(xml_file) for _ in range(5)]
+    adapt(query, recipes)
