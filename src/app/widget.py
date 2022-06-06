acee4b9a8e238bd735696b45cc9d5056ad317a56
Santiago Del Rey <59149491+santidrj@users.noreply.github.com>
2022-06-06 18:04:00 +0200

    Create files for UI

---
+++ src/app/widget.py
@@ -0,0 +1,40 @@
+# This Python file uses the following encoding: utf-8
+import os
+from pathlib import Path
+import sys
+
+from PySide6.QtWidgets import QApplication, QWidget, QLabel, QScrollArea, QCheckBox
+from PySide6.QtCore import QFile
+from PySide6.QtUiTools import QUiLoader
+
+
+class Widget(QWidget):
+    def __init__(self):
+        super(Widget, self).__init__()
+        self.load_ui()
+        self.init_ui()
+
+    def load_ui(self):
+        loader = QUiLoader()
+        path = os.fspath(Path(__file__).resolve().parent / "form.ui")
+        ui_file = QFile(path)
+        ui_file.open(QFile.ReadOnly)
+        loader.load(ui_file, self)
+        ui_file.close()
+
+    def init_ui(self):
+        scroll = self.findChild(QScrollArea, "drink_type").widget()
+        vbox = scroll.layout()
+        for i in range(10):
+            vbox.addWidget(QCheckBox("Text"))
+
+        scroll = self.findChild(QScrollArea, "glass_type").widget()
+        vbox = scroll.layout()
+        for i in range(10):
+            vbox.addWidget(QCheckBox("Text"))
+
+if __name__ == "__main__":
+    app = QApplication([])
+    widget = Widget()
+    widget.show()
+    sys.exit(app.exec())
