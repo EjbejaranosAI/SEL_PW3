263bac9700f549dc3bd54238614b0fa4001786c9
Santiago del Rey <59149491+santidrj@users.noreply.github.com>
2022-06-16 19:05:18 +0200

    Fix app import path

--- src/app/app.py
+++ src/app/app.py
@@ -20,7 +20,7 @@ from PySide6.QtWidgets import (
 from definitions import LOG_FILE, USER_MANUAL_FILE
 from entity.query import Query
 
-sys.path.append(os.fspath(Path(__file__).resolve().parent.parent))
+sys.path.append(os.fspath(Path(__file__).resolve().parent.parent.parent))
 
 from src.cbr.cbr import CBR
 
